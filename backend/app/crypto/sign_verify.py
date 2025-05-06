4ed26f2cbdb3a7664728eb9cbc7d076270ebfe19
AGM54 <134453750+AGM54@users.noreply.github.com>
2025-05-06 17:35:38 -0600

    Update sign_verify.py

--- backend/app/crypto/sign_verify.py
+++ backend/app/crypto/sign_verify.py
@@ -1,26 +1,19 @@
 from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives.asymmetric import padding
 
 # --- Sign a file using RSA Private Key ---
-def sign_file(file_path: str, private_key):
-	# Read file data
-	with open(file_path, "rb") as f:
-		file_data = f.read()
-
-	# Sign the file data
-	signature = private_key.sign(file_data, padding.PKCS1v15(), hashes.SHA256())
+def sign_data(data: bytes, private_key):
+	# Sign the file data using ECDSA with SHA-256
+	signature = private_key.sign(data, ec.ECDSA(hashes.SHA256()))
 
 	return signature
 
 # --- RSA Signature Verification ---
-def verify_signature(file_path: str, signature: bytes, public_key):
-	# Read file data
-	with open(file_path, "rb") as f:
-		file_data = f.read()
-
+def verify_signature(data: bytes, signature: bytes, public_key):
 	try:
-		# Verify the signature
-		public_key.verify(signature, file_data, padding.PKCS1v15(), hashes.SHA256())
+		# Verify the signature using ECDSA with SHA-256
+		public_key.verify(signature, data, ec.ECDSA(hashes.SHA256()))
 		return True
 	except Exception as e:
 		# If the signature is invalid, an exception is raised
